274aeed833a3705b649afbbd654b6596babc8d35
macbook <macbook@MacBook-Air.local>
2025-05-23 12:50:01 +0100

    Updated one final image

--- src/Pages/HomePage/Main/FifthSection.jsx
+++ src/Pages/HomePage/Main/FifthSection.jsx
@@ -21,7 +21,7 @@ const FifthSection = () => {
         </div>
 
         <div className="shadow-xl rounded-md bg-white flex items-center px-3 cardhover max-sm: h-[4.5vh] sm:h-[4.5vh] lg:h-[7vh] ">
-          <img src="/public/image 29.png" alt="" className=" max-sm: w-[7vmax]  sm:w-[8vmax]  lg:w-[8vmax]"/>
+          <img src="/image%2029.png" alt="" className=" max-sm: w-[7vmax]  sm:w-[8vmax]  lg:w-[8vmax]"/>
         </div>
 
         <div className="">
